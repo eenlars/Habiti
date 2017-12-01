2e9d6c2c165419b09d03daec91e10f7dc015647d
Neel Manglani <neel738@hotmail.com>
2017-12-01 16:24:46 +0100

    added getters and setters for Habit object

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,19 +1,33 @@
 function Habit(title, notes, frequency){
     this.title = title;
-    this.notes = notes;
-    this.frequency = frequency
+    /*
+	tags, difficulty , points, status, completed
+
+
+
+    */
 }
 
 Habit.prototype.setTitle = function(t){this.title = t;};
+
+
 Habit.prototype.setNote = function(t){this.note = t;};
 Habit.prototype.setFrequency = function(t){this.frequency = t;};
+Habit.prototype.setTags = function(t){this.tags = t;};
+Habit.prototype.setDifficulty = function(t){this.difficulty = t;};
+Habit.prototype.setPoints = function(t){this.points = t;};
+Habit.prototype.setStatus = function(t){this.status = t;};
+Habit.prototype.setCompleted = function(t){this.completed = t;};
+
+
+
 Habit.prototype.getTitle = function(){return this.title;};
-
-var h1 = new Habit("Test", "This is a test Habit" , 3);
-function onClick(){
-	console.log(h1.getTitle());
-	alert("Hello");
-}
-
+Habit.prototype.getNotes = function(){return this.notes;};
+Habit.prototype.getFrequency = function(){return this.frequency;};
+Habit.prototype.getPoints = function(){return this.points;};
+Habit.prototype.getStatus = function(){return this.status;};
+Habit.prototype.getCompleted = function(){return this.completed;};
+Habit.prototype.getTags = function(){return this.tags;};
+Habit.prototype.getDifficulty = function(){return this.difficulty;};
 
 
