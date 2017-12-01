386a44728b2d35dfe9ded54a8191fea968baa568
Neel Manglani <neel738@hotmail.com>
2017-12-01 15:23:21 +0100

    added two basic objects Habit and ToDo

---
+++ javascript/objects.js
@@ -0,0 +1,20 @@
+var habit ={
+    title: "",
+    notes: "",
+    tags: [],
+    frequency: 0,
+    points: 0,
+    status: "",
+    completed: false,
+    difficulty: 0,
+
+
+}
+
+var todo  = {
+    title: "",
+    tags: [],
+    completed: false,
+    deadline: Date,
+
+}
